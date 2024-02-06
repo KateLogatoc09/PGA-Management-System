b70007320df94b13ce62c3d67062381243f0e05e
KateLogatoc09 <144669024+KateLogatoc09@users.noreply.github.com>
2024-02-06 11:35:18 +0800

    delete
    delete

--- public/assets/js/book.js
+++
@@ -1,6 +0,0 @@
-// Include this file in your HTML after jQuery and Bootstrap JS
-
-$(document).ready(function() {
-    // Your additional JavaScript code can go here
-    // (if you have any specific functionality for the modal)
-});
